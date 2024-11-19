b7b3d42dd5781a52f8f0fae5e8f17523a7fe7683
Zineb-Sordo <zinebsordo@hotmail.fr>
2024-11-19 10:05:51 -0600

    refactoring of util functions

--- rhizonet/predict2d.py
+++ rhizonet/predict2d.py
@@ -64,7 +64,7 @@ def pred_function(image, model, pred_patch_size):
 
 
 def predict_step(image_path, model, pred_patch_size):
-    image, img_path = transform_image(image_path)
+    image, img_path = transform_image(image_path).unsqueeze(0)
     # cropped_image = extract_largest_component_bbox_image(image.unsqueeze(0), lab=None,  predict=True)
     logits = pred_function(image, model, pred_patch_size)
     pred = torch.argmax(logits, dim=1).byte().squeeze(dim=1)
