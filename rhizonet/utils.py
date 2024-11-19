7179e5f799c8fef2b78c55a7fd69f0eeb1424309
Zineb-Sordo <zinebsordo@hotmail.fr>
2024-11-19 10:25:41 -0600

    refactoring of util functions

--- rhizonet/utils.py
+++ rhizonet/utils.py
@@ -73,8 +73,7 @@ def extract_largest_component_bbox_image(img, lab=None, predict=False):
             new_image = np.zeros_like(img)
             # Applying the mask without cropping out the ROI 
             new_image[..., min_row:max_row, min_col:max_col] = cropped_image * filled_largest_component_mask[min_row:max_row, min_col:max_col]
-            return torch.Tensor(new_image)
-            # return new_image
+            return torch.Tensor(new_image).to("cuda") 
         else:
             # Create a new image with the cropped content
             new_image = np.zeros_like(cropped_image)
