16b727278ebcaadbd2f428020f6e6ef4a8333f78
Zineb-Sordo <zinebsordo@hotmail.fr>
2024-11-26 13:53:11 -0600

    added option to specify labels in groundtruth e.g. [0,81, 175], debugging --> make sure the labels are the correct ones

--- rhizonet/unet2D.py
+++ rhizonet/unet2D.py
@@ -434,7 +434,7 @@ class Unet2D(pl.LightningModule):
             preds = (preds * 255).byte()
 
 
-            y = MapImage(preds, (list(range(len(self))), self.hparams.class_values))
+            y = MapImage(preds, (list(range(len(self.hparams.class_values))), self.hparams.class_values))
             
             gridy = torchvision.utils.make_grid(y.view(y.shape[0], 1, y.shape[1], y.shape[2]), nrow=5)
             self.logger.log_image(key="prediction_imgs", images=[to_pil(gridy)]) # Wandb Logger
